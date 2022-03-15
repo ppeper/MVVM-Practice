c739d465e4ab4ed02d6f8a95a5535cc2ba798fd8
Joonhoo <mnc5@naver.com>
2022-03-15 16:49:58 +0900

    안드로이드 Navigation graph 사용하여 기본화면 구성

--- NewsApiClient/app/src/main/java/com/kyonggi/newsapiclient/MainActivity.kt
+++ NewsApiClient/app/src/main/java/com/kyonggi/newsapiclient/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kyonggi.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kyonggi.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(
+            navController
+        )
     }
 }

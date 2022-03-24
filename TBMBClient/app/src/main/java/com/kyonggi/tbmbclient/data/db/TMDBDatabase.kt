46d0d6c7c71503751b2b7d363c8ed91fd04effdc
Joonhoo <mnc5@naver.com>
2022-03-24 13:57:22 +0900

    RoomDatabase 상속 추가

--- TBMBClient/app/src/main/java/com/kyonggi/tbmbclient/data/db/TMDBDatabase.kt
+++ TBMBClient/app/src/main/java/com/kyonggi/tbmbclient/data/db/TMDBDatabase.kt
@@ -1,6 +1,7 @@
 package com.kyonggi.tbmbclient.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.kyonggi.tbmbclient.data.model.artist.Artist
 import com.kyonggi.tbmbclient.data.model.movie.Movie
 import com.kyonggi.tbmbclient.data.model.tvshow.TvShow
@@ -10,7 +11,7 @@ import com.kyonggi.tbmbclient.data.model.tvshow.TvShow
     version = 1,
     exportSchema = false
 )
-abstract class TMDBDatabase {
+abstract class TMDBDatabase: RoomDatabase() {
     abstract fun movieDao(): MovieDao
     abstract fun tvDao(): TvShowDao
     abstract fun artistDao(): ArtistDao

a77a4d3446d04a977b20145c96e2b1f26f2872a4
Joonhoo <mnc5@naver.com>
2022-03-28 17:32:01 +0900

    artist, tvshow repositoryImpl 추가

--- TBMBClient/app/src/main/java/com/kyonggi/tbmbclient/data/repository/artist/ArtistRepositoryImpl.kt
+++ TBMBClient/app/src/main/java/com/kyonggi/tbmbclient/data/repository/artist/ArtistRepositoryImpl.kt
@@ -1,14 +1,72 @@
 package com.kyonggi.tbmbclient.data.repository.artist
 
+import android.util.Log
 import com.kyonggi.tbmbclient.data.model.artist.Artist
+import com.kyonggi.tbmbclient.data.repository.artist.datasource.ArtistCacheDataSource
+import com.kyonggi.tbmbclient.data.repository.artist.datasource.ArtistLocalDataSource
+import com.kyonggi.tbmbclient.data.repository.artist.datasource.ArtistRemoteDataSource
 import com.kyonggi.tbmbclient.domain.repository.ArtistRepository
 
-class ArtistRepositoryImpl(): ArtistRepository {
-    override suspend fun getArtist(): List<Artist>? {
-        TODO("Not yet implemented")
+class ArtistRepositoryImpl(
+    private val artistRemoteDataSource: ArtistRemoteDataSource,
+    private val artistLocalDataSource: ArtistLocalDataSource,
+    private val artistCacheDataSource: ArtistCacheDataSource
+): ArtistRepository {
+    override suspend fun getArtist(): List<Artist> {
+        return getArtistsFromCache()
     }
 
-    override suspend fun updateArtist(): List<Artist>? {
-        TODO("Not yet implemented")
+    override suspend fun updateArtist(): List<Artist> {
+        val newListOfArtists = getArtistsFromAPI()
+        artistLocalDataSource.clearAll()
+        artistLocalDataSource.saveMoviesToDB(newListOfArtists)
+        artistCacheDataSource.saveArtistsToCache(newListOfArtists)
+        return newListOfArtists
+    }
+
+    suspend fun getArtistsFromAPI(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            val response = artistRemoteDataSource.getArtists()
+            val body = response.body()
+            if (body != null) {
+                artistList = body.artists
+            }
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        return artistList
+    }
+
+    suspend fun getArtistsFromDB(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            artistList = artistLocalDataSource.getArtistsFromDB()
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        if (artistList.isNotEmpty()) {
+            return artistList
+        } else {
+            artistList = getArtistsFromAPI()
+            artistLocalDataSource.saveMoviesToDB(artistList)
+        }
+        return artistList
+    }
+
+    suspend fun getArtistsFromCache(): List<Artist> {
+        lateinit var artistList: List<Artist>
+        try {
+            artistList = artistCacheDataSource.getArtistsFromCache()
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        if (artistList.isNotEmpty()) {
+            return artistList
+        } else {
+            artistList = getArtistsFromDB()
+            artistCacheDataSource.saveArtistsToCache(artistList)
+        }
+        return artistList
     }
 }
---
+++ TBMBClient/app/src/main/java/com/kyonggi/tbmbclient/data/repository/tvshow/TvShowRepositoryImpl.kt
@@ -0,0 +1,73 @@
+package com.kyonggi.tbmbclient.data.repository.tvshow
+
+import android.util.Log
+import com.kyonggi.tbmbclient.data.model.tvshow.TvShow
+import com.kyonggi.tbmbclient.data.repository.tvshow.datasource.TvShowCacheDataSource
+import com.kyonggi.tbmbclient.data.repository.tvshow.datasource.TvShowLocalDataSource
+import com.kyonggi.tbmbclient.data.repository.tvshow.datasource.TvShowRemoteDataSource
+import com.kyonggi.tbmbclient.domain.repository.TvShowRepository
+import java.lang.Exception
+
+class TvShowRepositoryImpl(
+    private val tvShowRemoteDataSource: TvShowRemoteDataSource,
+    private val tvShowLocalDataSource: TvShowLocalDataSource,
+    private val tvShowCacheDataSource: TvShowCacheDataSource
+): TvShowRepository {
+    override suspend fun getTvShows(): List<TvShow> {
+        return getTvShowsFromCache()
+    }
+
+    override suspend fun updateTvShows(): List<TvShow> {
+        val newListOfTvShows = getTvShowsFromAPI()
+        tvShowLocalDataSource.clearAll()
+        tvShowLocalDataSource.saveTvShowsToDB(newListOfTvShows)
+        tvShowCacheDataSource.saveTvShowsToCache(newListOfTvShows)
+        return newListOfTvShows
+    }
+
+    suspend fun getTvShowsFromAPI(): List<TvShow> {
+        lateinit var tvShowList: List<TvShow>
+        try {
+            val response = tvShowRemoteDataSource.getTvShows()
+            val body = response.body()
+            if (body != null) {
+                tvShowList = body.tvShows
+            }
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        return tvShowList
+    }
+
+    suspend fun getTvShowsFromDB(): List<TvShow> {
+        lateinit var tvShowList: List<TvShow>
+        try {
+            tvShowList = tvShowLocalDataSource.getTvShowsFromDB()
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        if (tvShowList.isNotEmpty()) {
+            return tvShowList
+        } else {
+            tvShowList = getTvShowsFromAPI()
+            tvShowLocalDataSource.saveTvShowsToDB(tvShowList)
+        }
+        return tvShowList
+    }
+
+    suspend fun getTvShowsFromCache(): List<TvShow> {
+        lateinit var tvShowList: List<TvShow>
+        try {
+            tvShowList = tvShowCacheDataSource.getTvShowsFromCache()
+        } catch (e: Exception) {
+            Log.i("TAG", e.message.toString())
+        }
+        if (tvShowList.isNotEmpty()) {
+            return tvShowList
+        } else {
+            tvShowList = getTvShowsFromDB()
+            tvShowCacheDataSource.saveTvShowsToCache(tvShowList)
+        }
+        return tvShowList
+    }
+}
